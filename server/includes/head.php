b06f636fa2a7e2e733c61a0d4fc0b46cb644c51a
Kerry O'Connor <kerryoco@gmail.com>
2021-04-24 12:02:43 -0400

    mobile-test

--- server/includes/head.php
+++ server/includes/head.php
@@ -1,4 +1,8 @@
 <head>
+	<meta name='description' content='A tournament scheduling app'>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<link rel='stylesheet' href='<?php echo $env->public_root . "/client/css/manager.css"; ?>' >
 	<link rel='stylesheet' href='<?php echo $env->public_root . "/client/css/modal.css"; ?>' >
 	<link rel="icon" type="image/x-icon" href="<?php echo $env->public_root . '/resource/media/favicon.ico'; ?>" >
